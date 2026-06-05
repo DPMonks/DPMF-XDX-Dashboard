78d9ef4cbfa4e8a5f3ca1aefdf9dd0e8741bbd15
Daniel <daniel-ceo@dpmf.technology>
2026-06-05 10:05:02 +0100

    Add XDX/XRP and XDX/RLUSD pools

---
+++ src/pools.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: "XDX/XRP",
+    asset: {
+      currency: "XDX",
+      issuer: "rMJAXYsbNzhwp7FfYnAsYP5ty3R9XnurPo"
+    },
+    asset2: {
+      currency: "XRP"
+    },
+    issuer: "rMJAXYsbNzhwp7FfYnAsYP5ty3R9XnurPo"
+  },
+
+  {
+    name: "XDX/RLUSD",
+    asset: {
+      currency: "XDX",
+      issuer: "rMJAXYsbNzhwp7FfYnAsYP5ty3R9XnurPo"
+    },
+    asset2: {
+      currency: "RLUSD",
+      issuer: "rHqHaQf8rVj8Yk1Vx1t6gFj6FQx1Jp9z8C"   // RLUSD issuer
+    },
+    issuer: "rMJAXYsbNzhwp7FfYnAsYP5ty3R9XnurPo"
+  }
+];
