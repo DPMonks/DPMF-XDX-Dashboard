b7211c6fefdbf22ae7d105d696a8556a54910de7
Daniel <daniel-ceo@dpmf.technology>
2026-07-25 15:11:23 +0100

    Switch main.jsx to render App.jsx dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import XamanApp from "./XamanApp";
+import App from "./App";                 // ← your new dashboard UI
 import { WalletProvider } from "./context/WalletContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <WalletProvider>
-      <XamanApp />
+      <App />                           // ← render the neon dashboard
     </WalletProvider>
   </React.StrictMode>
 );
